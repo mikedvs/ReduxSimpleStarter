840fedf4a008428e39dd52b8b0b9e227a4207da8
mikedvs41 <michael.maliniak@dtdigital.com.au>
2017-04-30 19:40:36 +1000

    states

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,10 +4,16 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { term: ''};
+        this.state = { term: 'banana'};
     }
     render() {
-        return <input onChange={event => console.log(event.target.value)} />;
+        return ( 
+            <div>
+                <input 
+                    value = {this.state.term}
+                    onChange={event => this.setState({term: event.target.value})} />
+            </div>
+        );
     }
 }
 
