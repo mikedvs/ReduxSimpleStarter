9700753692f03110c75f9ac607742579a4aebf8c
Maliniak, Michael <Michael.Maliniak@news.com.au>
2017-05-01 15:05:23 +1000

    added video list items

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+//Youtube Search API
+import YTSearch from 'youtube-api-search'
+
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAvwCKvOSYt1Jx2iXlYc_tRmW5Ikj0UYYQ';
 
 // Create new component. This component should produce som
 // HTML
 
-const App = () => {
-    return <div>
-        <SearchBar />
-    </div>
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({
+			key: API_KEY,
+			term: 'surfboards'
+		}, (videos) => {
+			this.setState({ videos });
+			// equivalent to this.setState({videos:videos}); !Syntactic sugar
+		});
+	}
+    render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos}/>
+			</div>
+		);
+	} 
 }
 
 // Take this component's generated HTML and put it on 
