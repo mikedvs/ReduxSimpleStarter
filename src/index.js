0172c585ec09b24223da15bf9c6d565bd2185bd3
Salty.Vikings <developers@saltyvikings.com.au>
2018-08-28 14:40:14 +0800

    added actions folder and reducers

--- src/index.js
+++ src/index.js
@@ -1,46 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-//Youtube Search API
-import YTSearch from 'youtube-api-search'
+import App from './components/app';
+import reducers from './reducers';
 
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-const API_KEY = 'AIzaSyAvwCKvOSYt1Jx2iXlYc_tRmW5Ikj0UYYQ';
-
-// Create new component. This component should produce som
-// HTML
-
-class App extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = { videos: [] };
-
-		YTSearch({
-			key: API_KEY,
-			term: 'surfboards'
-		}, (videos) => {
-			this.setState({ videos });
-			console.log(videos);
-			// equivalent to this.setState({videos:videos}); !Syntactic sugar
-		});
-	}
-    render() {
-		return (
-			<div>
-				<SearchBar />
-				<VideoList videos={this.state.videos}/>
-			</div>
-		);
-	} 
-}
-
-// Take this component's generated HTML and put it on 
-// the page
-
-const container = document.querySelector('.container');
-
-ReactDOM.render(<App />, container);
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
 
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javavavascript: The Good Parts'},
+        {title: 'Jaws: The Good Part'},
+        {title: 'Surfing: The Good Parts'},
+        {title: 'Skating: The Good Parts'},
+    ]
+}
